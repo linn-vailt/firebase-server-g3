cffe43c3f4a45d46dd191b86868105f685b9870b
Linn Vailt <113286790+linn-vailt@users.noreply.github.com>
2023-09-29 09:43:09 +0200

    #54 PUT, GET and DELETE req

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
-var serviceAccount = require("./permissions.json");
+var serviceAccount = require("./permissions.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -15,43 +15,101 @@ const cors = require('cors')
 app.use( cors( {origin:true } ) )
 
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
 //Routes
-app.get('/hello-world', (req, res) => {
-    return res.status(200).send('Hello biches!!')
-})
+// app.get('/hello-world', (req, res) => {
+//     return res.status(200).send('Hello biches!!')
+// })
 
-//CREATE
-app.post('/create', (req, res) => {
+//CREATE a blogpost
+//POST
+const createBlogPost = async (req, res) => {
+    try {
+      const { title, content } = req.body
+      const newBlogPost = {
+        title,
+        content,
+      };
+      await db.collection('blogposts').add(newBlogPost)
+  
+      return res.status(201).json({ message: 'Blog post created successfully' })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: 'Server error' })
+    }
+  };
+  app.post('/create', createBlogPost)
 
-    (async () => {
-        try{
-            await db.collection('blogposts').doc('/' + req.body.id + '/')
-            .create({
-                title: req.body.title,
-                content: req.body.content,
-            })
-            return res.status(200).send()   
-        }
-        catch (error) {
-            console.log(error)
-            return res.status(500).send(error)
-        }
-    })()
-})
+//READ a specific product based on id
+//GET
+const getBlogById = async (req, res) => {
+    try {
+      const blogPostId = req.params.id
+      const blogPostRef = db.collection('blogposts').doc(blogPostId)
+      const doc = await blogPostRef.get()
+  
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Blog post not found' })
+      }
+  
+      const blogPostData = doc.data()
+      return res.status(200).json(blogPostData)
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: 'Server error' })
+    }
+  };
+app.get('/blogposts/:id', getBlogById)
 
 
-//READ
+// UPDATE a specific blog post based on ID
+// PUT
+const updateBlogById = async (req, res) => {
+    try {
+      const blogPostId = req.params.id
+      const { title, content } = req.body
+  
+      const blogPostRef = db.collection('blogposts').doc(blogPostId)
+      const doc = await blogPostRef.get()
+  
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Blog post not found' })
+      }
 
-
-//UPDATE
-
+      const updatedFields = { ...(title && { title }), ...(content && { content }) }
+  
+      await blogPostRef.update(updatedFields);
+  
+      return res.status(200).json({ message: 'Blog post updated successfully' })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: 'Server error' })
+    }
+  }
+  
+app.put('/blogposts/:id', updateBlogById)
+  
 
 //DELETE
-
-
+const deleteBlogById = async (req, res) => {
+    try {
+      const blogPostId = req.params.id;
+  
+      const blogPostRef = db.collection('blogposts').doc(blogPostId)
+      const doc = await blogPostRef.get()
+  
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Blog post not found' })
+      }
+  
+      await blogPostRef.delete()
+  
+      return res.status(204).send()
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: 'Server error' })
+    }
+  };
+app.delete('/blogposts/:id', deleteBlogById)
 
 
 exports.api = functions.https.onRequest(app)
