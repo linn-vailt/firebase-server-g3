7bf1514ac2bc573486c753f8bb7af056f3264997
AD1121 <sinagh30@gmail.com>
2023-10-02 18:06:42 +0200

    Tests added

--- functions/__tests__/component-test.js
+++ functions/__tests__/component-test.js
@@ -1,11 +1,69 @@
 /**
  * @group component
  */
- 
-describe('When testing jest', () => {
-	describe('given i have a non failing test', () => {
-		it('should be one', () => {
-			expect(1).toBe(1)
-		})
-	})
-})
+
+// describe('When testing jest', () => {
+// 	describe('given i have a non failing test', () => {
+// 		it('should be one', () => {
+// 			expect(1).toBe(1)
+// 		})
+// 	})
+// })
+
+const request = require("supertest");
+const app = require("../server");
+
+// POST create blog test
+describe("When testing /blogs", () => {
+  describe("POST", () => {
+    it("should work", async () => {
+      const res = await request(app)
+        .post("/blogs")
+        .send({ title: "Food", content: "Food Blog" });
+      expect(res.statusCode).toEqual(201);
+    });
+  });
+});
+
+// GET all blogs test
+describe("When testing /blogs", () => {
+  describe("GET", () => {
+    it("should work", async () => {
+      const res = await request(app).get("/blogs");
+      expect(res.statusCode).toEqual(200);
+      expect.arrayContaining(res.body);
+    });
+  });
+});
+
+// GET blog by ID test
+describe("When testing /blogs/:id", () => {
+  describe("GET", () => {
+    it("should work", async () => {
+      const res = await request(app).get("/blogs/65146916aabcbbdf613dd59e");
+      expect(res.statusCode).toEqual(200);
+    });
+  });
+});
+
+// PUT edit blog test
+describe("When testing /blogs/:id", () => {
+  describe("PUT", () => {
+    it("should work", async () => {
+      const res = await request(app)
+        .put("/blogs/65146916aabcbbdf613dd59e")
+        .send({ title: "Food Blog", content: "New Content" });
+      expect(res.statusCode).toEqual(200);
+    });
+  });
+});
+
+// DELETE blog test
+describe("When testing /blogs/65146916aabcbbdf613dd59e", () => {
+  describe("DELETE", () => {
+    it("should work", async () => {
+      const res = await request(app).delete("/blogs/65146916aabcbbdf613dd59e");
+      expect(res.statusCode).toEqual(204);
+    });
+  });
+});
--- functions/__tests__/integration-test.js
+++ functions/__tests__/integration-test.js
@@ -1,11 +1,72 @@
 /**
  * @group integration
  */
- 
-describe('When testing jest', () => {
-	describe('given i have a non failing test', () => {
-		it('should be one', () => {
-			expect(1).toBe(1)
-		})
-	})
-})
+
+// describe('When testing jest', () => {
+// 	describe('given i have a non failing test', () => {
+// 		it('should be one', () => {
+// 			expect(1).toBe(1)
+// 		})
+// 	})
+// })
+
+const request = require("supertest");
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || `http://localhost:${PORT}`;
+const container = request(HOST);
+
+// GET create blog test
+describe("When testing /blogs", () => {
+  describe("POST", () => {
+    it("should work", async () => {
+      const res = await container.post("/blogs").send({
+        title: "Integration test title",
+        content: "Integration test content",
+      });
+      expect(res.statusCode).toEqual(201);
+    });
+  });
+});
+
+// GET all blogs test
+describe("When testing /blogs", () => {
+  describe("GET (All)", () => {
+    it("should work", async () => {
+      const res = await container.get("/blogs");
+      expect(res.statusCode).toEqual(200);
+      expect.arrayContaining(res.body);
+    });
+  });
+});
+
+// GET blog by ID test
+describe("When testing /blogs/:id", () => {
+  describe("GET", () => {
+    it("should work", async () => {
+      const res = await container.get("/blogs/65148402cfa32d8684c8010d");
+      expect(res.statusCode).toEqual(200);
+    });
+  });
+});
+
+// PUT edit blog test
+describe("When testing /blogs/:id", () => {
+  describe("PUT", () => {
+    it("should work", async () => {
+      const res = await container
+        .put("/blogs/65148402cfa32d8684c8010d")
+        .send({ title: "Int edited Title", content: "Int edited Content" });
+      expect(res.statusCode).toEqual(200);
+    });
+  });
+});
+
+// DELETE blog test
+describe("When testing /blogs/:id", () => {
+  describe("DELETE", () => {
+    it("should work", async () => {
+      const res = await container.delete("/blogs/651486de7ec1ab1e5fedbf62");
+      expect(res.statusCode).toEqual(200);
+    });
+  });
+});
